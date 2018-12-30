633a50607dd4eb429fd181a3570174d12c394639
salmaakterdolly <akter.myself85@gmail.com>
2018-12-29 22:02:07 -0500

    tesrrunner

---
+++ src/test/java/com/my/testRunner/Testrunner.java
@@ -0,0 +1,14 @@
+package com.my.testRunner;
+
+import cucumber.api.CucumberOptions;
+
+@CucumberOptions(
+features = {"Features"}
+//glue = {"com.my.steps"}
+//tags = {"@Registration"}
+
+)
+public class Testrunner {// extends AbstructTestNGCucumberTest {
+	
+
+}
