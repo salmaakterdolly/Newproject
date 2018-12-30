3f2f510bbd7bcb71486fdc981b22b611c36128e8
salma akter <akter.myself85@gmail.com>
2018-12-30 14:43:44 -0500

    https://github.com/salmaakterdolly/Newproject.git

---
+++ src/test/java/com/my/steps/userRgistration.java
@@ -0,0 +1,28 @@
+package com.my.steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class userRgistration {
+	@Given("^as a user i am on www\\.macys\\.com registration page\\.$")
+	public void as_a_user_i_am_on_www_macys_com_registration_page() throws Throwable {
+	    
+	}
+
+	@When("^I enter First name,last name,user id ,password, confirm password$")
+	public void i_enter_First_name_last_name_user_id_password_confirm_password() throws Throwable {
+	    
+	}
+
+	@When("^i click on create account button$")
+	public void i_click_on_create_account_button() throws Throwable {
+	   
+	}
+
+	@Then("^i received a user registration confirmation$")
+	public void i_received_a_user_registration_confirmation() throws Throwable {
+	   
+	}
+
+}
--- src/test/java/com/my/testRunner/TestRunner.java
+++ src/test/java/com/my/testRunner/TestRunner.java
@@ -3,17 +3,17 @@ package com.my.testRunner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-public class TestRunner {
+
 	@CucumberOptions(
 			features = {"Features"},
 			glue = {"com.my.steps"},
 			tags = {"@Registration"}
 
 			)
-			public class Testrunner extends AbstractTestNGCucumberTests {
+			public class TestRunner extends AbstractTestNGCucumberTests {
 				
 
 			}
 
 
-}
+
